9a819ea7ae4210b1cb491af95543ea2e61cbc76d
cminmins <41716029+cminmins@users.noreply.github.com>
2018-08-03 19:19:29 +0900

    Revert "asd"
    This reverts commit 00b3cadf361e6cc7ca8db8b1f0f67eda6857b551.

--- Algorithm/삼성SDS B1번 보물찾기.cpp
+++ Algorithm/삼성SDS B1번 보물찾기.cpp
@@ -1,53 +1,58 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <string.h>
 using namespace std;
-bool arr[1001][1001] = {0};
-//vector<int> arr[1001];
-bool visit[1001] = { 0 };
-int cnt = 0;
-int min_time = 10001;
-
-void dfs(int s, int d, int time)
-{
-		visit[s] = true;
-		if (s == d)
-		{
-			if (min_time >= time)
-				min_time = time;
-		}
-
-		for (int i = 1; i <= d; i++)
-			if(arr[s][i] == true && (visit[i] == false || i ==d))
-				dfs(i, d, time+1);
-
-}
-
-
+queue<int> bfs_queue;
+bool visit[1001] = { false };
+bool map[1001][1001] = { false };
+int dist[1001] = { -1 };
 
 int main()
 {
-	int t; int n, m, k;
+	int t, n, m, k;
 	cin >> t;
+
 	for (int i = 1; i <= t; i++)
 	{
 		cin >> n >> m >> k;
-		memset(arr, 0, sizeof(arr));
 		memset(visit, 0, sizeof(visit));
+		memset(map, 0, sizeof(map));
+		for (int k = 0; k <= n; k++)
+			dist[k] = -1;
 
 		for (int a = 0; a < m; a++)
 		{
-			int t1 = 0, t2 = 0;
-			cin >> t1 >> t2;
-			arr[t1][t2] = true;
+			int temp1, temp2;
+			cin >> temp1 >> temp2;
+			map[temp1][temp2] = true;
 		}
 
-		dfs(1, n, 0);
-		if(min_time > k)
+		bfs_queue.push(1);
+		dist[0] = 0;
+		dist[1] = 0;
+		while (!bfs_queue.empty())
+		{
+			int node = bfs_queue.front(); bfs_queue.pop();
+			if (visit[node] == false)
+			{
+				visit[node] = true;
+				for (int b = 1; b <= n; b++)
+					if (map[node][b] == true && visit[b] == false)
+					{
+						bfs_queue.push(b);
+						if(dist[b] == -1)
+							dist[b] = dist[node] + 1;
+					}
+			}
+		}
+		if(dist[n] > k)
 			cout << "#" << i << " " << -1 << endl;
-		else 
-			cout << "#" << i << " " << min_time << endl;
-		min_time = 10001;
+		else
+			cout << "#" << i << " " << dist[n] << endl;
 	}
+
+
+
+
 	return 0;
 }
